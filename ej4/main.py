68d602e782b09e76af3fa798c72124df1f6e15c1
Jmpardo047 <140624378+Jmpardo047@users.noreply.github.com>
2024-03-06 13:28:28 -0500

    commit ej4-2

--- ej4/main.py
+++ ej4/main.py
@@ -1,26 +1,32 @@
 #programa que permite registrar la información de los empleadosDe una compañía 
 #y le permita calcular el valor a pagar por concepto de nomina a Cada empleado.
-import os
-import sys
 from tabulate import tabulate
 import modules.corefiles as c
+import modules.empleados as emp
 if __name__ == '__main__':
+    info = c.Verifydata('compañia.json')
+    empleados,colillas = info.values()
     def MainMenu():
         def Excecute(func):
-            os.system('cls')
             func
             MainMenu()
-        info = c.Verifydata('compañia.json')
-        empleados,colillas = info.values()
         menu =[['1.', 'Registrar info de empleado'], ['2.', 'Calcular valor a pagar a empleado'], ['3.', 'Salir']]
+        c.os.system('cls')
         print(tabulate(menu, tablefmt='grid'))
         op = input('\n)..'  )
         if (op == '1'):
-            pass
+            c.os.system('cls')
+            Excecute(emp.AddEmpleado(empleados))
         elif (op == '2'):
-            pass
+            c.os.system('cls')
+            Excecute(emp.RegSalario(empleados,colillas))
         elif (op == '3'):
-            sys.exit('Hasta Pronto!')
+            finalUpdate = {
+                'empleados':empleados,
+                'colillas':colillas
+            }
+            c.UpdateFile('compañia.json',finalUpdate)
+            c.sys.exit('Hasta Pronto!')
         else:
             MainMenu()
     MainMenu()
---
+++ ej4/modules/empleados.py
@@ -0,0 +1,83 @@
+import modules.corefiles as c
+def AddEmpleado(empleados:dict):
+    print('---- AGREGAR INFORMMACIÓN DE EMPLEADO ----')
+    print('Ingrese el id del empleado')
+    id = str(c.ValidInt())
+    print('Ingrese el nombre del empleado')
+    name = c.Validstr()
+    cargo = MenuCargos()
+    print('Ingrese el sotck mínimo del producto')
+    nwEmp = {
+        'id': id,
+        'nombre': name,
+        'cargo': cargo,
+        'salario':{}
+    }
+    empleados.update({id:nwEmp})
+
+def MenuCargos():
+    c.os.system('cls')
+    print('Ingrese el cargo del empleado')
+    print('1. Almacenista\n2. Jefe IT\n3. Administrador\n4. Mensajero\n5. Gerente')
+    n = c.Validstr()
+    if (n == '1'):
+        rol = 'Almacenista'
+        return rol
+    elif (n == '2'):
+        rol = 'Jefe IT'
+        return rol
+    elif (n == '3'):
+        rol = 'Administrador'
+        return rol
+    elif (n == '4'):
+        rol = 'Mensajero'
+        return rol
+    elif (n == '5'):
+        rol = 'Gerente'
+        return rol
+    else:
+        return MenuCargos()
+
+def RegSalario(empleados:dict,colillas:dict):
+    print('ingrese el id del empleado a quien se le calculará el salario')
+    id = ValidEmp(empleados)
+    if (bool(id) == True):
+        route = empleados.get(id)
+        print('Ingrese el valor por dia')
+        valueDia = c.ValidFloat()
+        print('Ingrese la cantidad de días trabajados')
+        diasTotal = c.ValidInt()
+        print('Ingrese la cantidad de horas extra trabajadas')
+        horasExtra = c.ValidInt()
+        print('Ingrese el descuento por cafetería')
+        desCafe = c.ValidFloat()
+        print('Ingrese la cuota de prestamo')
+        prestamo = c.ValidFloat()
+        print('Ingrese el mes en que se realizará el pago')
+        mes = c.Validstr()
+        print('Ingrese la fecha en que se realizará el pago')
+        date = c.Validstr()
+        totalExtra = horasExtra*5500
+        sueldoBase = valueDia*diasTotal
+        totalPagar = (((sueldoBase)+totalExtra)-(desCafe+prestamo))
+        route['salario'] = totalPagar
+        nwPago = {
+            'mes':mes,
+            'fecha':date,
+            'sueldo base':sueldoBase,
+            'pago horas extra': totalExtra,
+            'cuota prestamo': prestamo,
+            'descuento cafeteria': desCafe,
+            'total': totalPagar
+        }
+        colillas.update({id:nwPago})
+    else:
+        pass
+
+def ValidEmp(empleados:dict):
+    id = str(c.ValidInt())
+    if (id in empleados.keys()):
+        return id
+    else:
+        print('El id de empleado ingresado no se encuentra registrado')
+        c.os.system('pause')
