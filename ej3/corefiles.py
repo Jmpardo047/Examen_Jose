339e2ae3e345d137590cb4e5a2a318d2c83d9b54
Jmpardo047 <140624378+Jmpardo047@users.noreply.github.com>
2024-03-06 12:13:48 -0500

    commitEj3

--- ej3/corefiles.py
+++ ej3/corefiles.py
@@ -1,22 +1,34 @@
-def AddProduct(productos):
+import json
+import os
+import sys
+BASE = 'data/'
+def AddProduct(productos:dict):
     print('---- AGREGAR PRODUCTO A LA TIENDA ----')
     print('Ingrese el id del producto')
     id = ValidInt()
     print('Ingrese el nombre del producto')
     name = Validstr()
-    print('Ingrese los apellidos del usuario')
-    lastName = Validstr()
-    print('Ingrese la ciudad donde vive el usuario')
-    ciudad = Validstr()
-    print('Ingrese la dirección donde reside el usuario')
-    direccion = Validstr()
-    print('Ingrese longitud de la ubicación del usuario')
-    longitud = ValidFloat()
+    print('Ingrese el valor unitario del prducto')
+    valor = ValidFloat()
+    print('Ingrese el sotck mínimo del producto')
+    stockMin = ValidInt()
+    print('Ingrese el stock máximo del producto')
+    stockMax = ValidInt()
+    nwProduct = {
+        'id': id,
+        'nombre': name,
+        'valor unitario': valor,
+        'stock minimo' : stockMin,
+        'stock maximo': stockMax
+    }
+    productos.update({id:nwProduct})
+
 
 def ValidInt():
     try:
         n = int(input(')..'))
     except ValueError:
+        DelLine()
         return ValidInt()
     else:
         return n
@@ -25,6 +37,7 @@ def ValidFloat():
     try:
         n = float(input(')..'))
     except ValueError:
+        DelLine()
         return ValidFloat()
     else:
         return n
@@ -34,4 +47,30 @@ def Validstr():
     if(bool(n) == True):
         return n
     else:
+        DelLine()
         return Validstr()
+
+def DelLine():
+    sys.stdout.write("\033[F")
+    sys.stdout.write("\033[K")
+
+def Verifydata(archivo,data):
+    if (CheckFile(archivo) == True):
+        return ReadFile(archivo)
+    else:
+        UpdateFile(archivo,data)
+        return ReadFile(archivo)    
+    
+def CheckFile(archivo):
+    if ((os.path.isfile(BASE+archivo))):
+        return True
+    else:
+        return False
+
+def ReadFile(archivo):
+    with open(BASE+archivo,'r') as rf:
+        return json.load(rf)
+    
+def UpdateFile(archivo,data):
+    with open(BASE+archivo,'w') as wf:
+        json.dump(data,wf,indent=4)
--- ej3/main.py
+++ ej3/main.py
@@ -1,3 +1,14 @@
 # programa en Python que permita registrar los productos de una Tienda de viveres.
+import corefiles as c
 if  __name__ == "__main__":
-    productos = {}
+    nwDc = {}
+    productos = c.Verifydata('inventario.json',nwDc)
+    rp = True
+    while rp:
+        c.os.system('cls')
+        c.AddProduct(productos)
+        rp = bool(input('desea agregar otro producto?, S(Si) o Enter(No)'))
+    c.UpdateFile('inventario.json',productos)
+    
+
+
